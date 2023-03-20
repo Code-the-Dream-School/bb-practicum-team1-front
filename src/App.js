0334dad96419574e695e11c8d007327429e61778
bonivan <ibondarev@luxoft.com>
2023-03-19 23:03:49 -0400

    Setup React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { getAllData } from './util/index';
+import { HomePage } from './components/HomePage/HomePage';
+import { LoginPage } from './components/LoginPage/LoginPage';
 
 const URL = 'http://localhost:8000/api/v1/';
 
-function App() {
+const App = () => {
 
 const [message, setMessage] = useState(''); 
 
@@ -20,9 +24,34 @@ const [message, setMessage] = useState('');
 
   }, []);
 
+  const ProtectedRoute = ({ children }) => {
+    // Placeholder to actually check if user is authenticated
+    const useAuth = () => {
+      if (Math.random() > 0.5) {
+        return true;
+      }
+      return false;
+    }
+
+    const user = useAuth();
+    if (!user) {
+      // User is not authenticated
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
   return (
     <>
       <h1>{message}</h1>
+      <ProtectedRoute>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+        </Routes>  
+      </ProtectedRoute> 
+      <Routes>
+        <Route  path="/login" element={<LoginPage />} />
+      </Routes> 
     </>
   );
 }
