2b63fd2490f494d943b99dec77a84d3d4e89cff5
koral14 <105457134+koral14@users.noreply.github.com>
2023-03-25 11:42:32 -0700

    removed the .scss files and returned the code to work with styled components

--- src/components/Header/Burger.jsx
+++ src/components/Header/Burger.jsx
@@ -1,63 +1,58 @@
 import React, { useState } from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import RightNav from './RightNav';
-import "./_Burger.scss";
 
-// const StyledBurger = styled.div`
-//     width: 2rem;
-//     height: 2rem;
-//     position: fixed;
-//     top: 25px;
-//     right: 20px;
+const StyledBurger = styled.div`
+    width: 2rem;
+    height: 2rem;
+    position: fixed;
+    top: 25px;
+    right: 20px;
     
-//     z-index: 20;
-//     display: none;
+    z-index: 20;
+    display: none;
 
-//     @media (max-width: 768px) {
-//         display: flex;
-//         justify-content: space-around;
-//         flex-flow: column nowrap;    }
+    @media (max-width: 768px) {
+        display: flex;
+        justify-content: space-around;
+        flex-flow: column nowrap;    }
 
-//     div {
-//         width: 2rem;
-//         height: 0.25rem;
-//         background-color: ${({ open }) => open ? '#ccc' : '#333'};
-//         border-radius: 10px;
-//         transform-origin: 1px;
-//         transition: all 0.3s linear;
-//         cursor: pointer;
+    div {
+        width: 2rem;
+        height: 0.25rem;
+        background-color: ${({ open }) => open ? '#ccc' : '#333'};
+        border-radius: 10px;
+        transform-origin: 1px;
+        transition: all 0.3s linear;
+        cursor: pointer;
 
-//         &:nth-child(1) {
-//             transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
-//         }
+        &:nth-child(1) {
+            transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
+        }
 
-//         &:nth-child(2) {
-//             transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0%)'};
-//             opacity: ${({ open }) => open ? 0 : 1};
-//         }
+        &:nth-child(2) {
+            transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0%)'};
+            opacity: ${({ open }) => open ? 0 : 1};
+        }
 
-//         &:nth-child(3) {
-//             transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
-//         }
-//     }
-// ` 
+        &:nth-child(3) {
+            transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
+        }
+    }
+` 
 
 const Burger = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className='burger'>
-            <div className={`${isOpen ? 'openClassBurger' : 'closeClassBurger'}`} open={isOpen} onClick={() => setIsOpen(!isOpen)} >
-                <div/>
-                <div/>
-                <div/>
-                <h3>test</h3>
-                <h3>test</h3>
-                <h3>test</h3>
-            </div>
-           
+        <>
+            <StyledBurger open={isOpen} onClick={() => setIsOpen(!isOpen)}>
+                <div className='burger-layer'/>
+                <div className='burger-layer'/>
+                <div className='burger-layer'/>
+            </StyledBurger>
             <RightNav open={isOpen} />
-        </div>
+        </>
     )
 }
 
--- src/components/Header/RightNav.jsx
+++ src/components/Header/RightNav.jsx
@@ -1,51 +1,51 @@
 import React, { useState } from 'react';
 import { forLoggedInUser, forSignedOutUser } from '../../constants';
-import "./_RightNav.scss";
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
-// const Ul = styled.ul`
-//   list-style: none;
-//   display: flex;
-//   flex-flow: row nowrap;
-//   margin-top: 0;
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  margin-top: 0;
 
-//   li {
-//     padding: 28px 15px;
-//     font-size: 1.7rem;
-//   }
+  li {
+    padding: 28px 15px;
+    font-size: 1.7rem;
+  }
 
-//   @media (max-width: 768px) {
-//     flex-flow: column nowrap;
-//     background-color: black;
-//     position: fixed;
-//     transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
-//     top: 0;
-//     right: 0;
-//     height: 100vh;
-//     width: 300px;
-//     padding-top: 3.5rem;
-//     transition: transform 0.3s ease-in-out;
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: black;
+    position: fixed;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
 
-//     li {
-//       color: #fff;
-//       cursor: pointer;
-//     }
-//   }
-// `;
+    li {
+      color: #fff;
+      cursor: pointer;
+    }
+  }
+`;
 
 const RightNav = ({ open }) => {
   // created only for being able to impliment the if
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <div className='nav-list'>
-      <ul open={open} className={`${open ? 'openClassR' : 'closeClassR'}`} >
+    <>
+      {/* Left this lines here for a visual effect because the ones above do not display anything yet */}   
+      <Ul open={open} >
         <li>Home</li>
         <li>About</li>
         <li>Contact</li>
         <li>Sign In</li>
         <li>Sign Up</li>
-      </ul>
+      </Ul>
       {/* map through the array with routes and route names and returning the correspondig page*/}
       {/* map through the array with an authenticated and unauthenticated user and returning the correspondig page*/}
 
@@ -62,9 +62,7 @@ const RightNav = ({ open }) => {
           </ul>
         })
       )}
-        {/* Left this lines here for a visual effect because the ones above do not display anything yet */}
-          
-    </div>
+    </>
   )
 }
 
