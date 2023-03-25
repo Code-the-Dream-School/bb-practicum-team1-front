119ecd7c9c7c3ef0c25595b7c384fb879f02bb2f
Cecilia Rodriguez <cecerodrgz@Cecilias-Air.lan>
2023-03-24 22:02:04 -0400

    added url line 9

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { LoginPage } from './components/LoginPage/LoginPage';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import {setCookie, getCookie, deleteCookie} from './util/Authentication';
 
+const URL = 'http://localhost:8000/api/v1/';
 
 function App() {
   const [message, setMessage] = useState("");
@@ -22,24 +23,6 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <form >
-  
-        <h1>{message}</h1>
-        <label>
-          <p> User Name: </p>
-          <input/>
-        </label>
-        <label>
-          <p> Password</p>
-          <input/>
-          <br />
-        </label>
-        <div>
-          <button type="submit"> Submit </button>
-        </div>
-      </form>
-</div>
     <>
       <h1>{message}</h1>
       
