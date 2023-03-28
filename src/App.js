a00a66597b7f595b20acd1a20a6a85e91d7328c5
SimonjShurety <simonjshurety@gmail.com>
2023-03-28 14:59:44 -0300

    Added requested changes

--- src/App.js
+++ src/App.js
@@ -1,82 +1,85 @@
-import React, { useState, useEffect, createContext } from "react";
-import TextInput from "./components/inputs/TextInput";
-import { Routes, Route } from 'react-router-dom';
-import { getAllData } from "./util/index";
-import Dropdowninput from "./components/inputs/DropdownInput";
-import { HomePage } from './components/HomePage/HomePage';
-import { LoginPage } from './components/LoginPage/LoginPage';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
-import './sass/app.scss';
-export const inputContext = createContext({});
+import React, { useState, useEffect, createContext } from 'react'
+import { getAllData } from './util/index'
+import { LoginPage } from './components/LoginPage/LoginPage'
+import { Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
+import { HomePage } from './components/HomePage/HomePage'
+import './sass/app.scss'
+export const InputContext = createContext({})
 
-
-const URL = "http://localhost:8000/api/v1/";
+const URL = 'http://localhost:8000/api/v1/'
 
 const App = () => {
-  const [message, setMessage] = useState("");
-  const [inputs, setInputs] = useState({});
+    const [message, setMessage] = useState('')
+    const [inputs, setInputs] = useState({})
 
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
-  ];
+    /* EXAMPLE: DropdownInput selection options
+   
+   const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' },
+    ]
+    */
 
-  useEffect(() => {
-    (async () => {
-      const myData = await getAllData(URL);
-      setMessage(myData.data);
-    })();
+    useEffect(() => {
+        ;(async () => {
+            const myData = await getAllData(URL)
+            setMessage(myData.data)
+        })()
 
-    return () => {
-      console.log("unmounting");
-    };
-  }, []);
+        return () => {
+            console.log('unmounting')
+        }
+    }, [])
 
-  return (
-    <>
-      <h1>{message}</h1>
-      <inputContext.Provider
-        value={{
-          inputs,
-          handleInputChange: (inputName, inputValue) =>
-            setInputs({ ...inputs, [inputName]: inputValue }),
-        }}
-      >
-        <TextInput
-          label="Text Input"
-          id="testInput"
-          type="text"
-          placeholder="Enter text here"
-          textarea={false}
-        />
-        <TextInput
-          label="Text Area"
-          id="textArea"
-          type="textarea"
-          placeholder="Enter text here"
-          textarea={true}
-        />
-        <Dropdowninput
-          label="Dropdown Menu"
-          id="DropdownMenu"
-          options={options}
-        />
-      </inputContext.Provider>
-      
-      <Routes>
-        <Route 
-          exact path="/" 
-          element={
-            <ProtectedRoute>
-              <HomePage />
-            </ProtectedRoute>
-          } 
-        />
-        <Route  path="/login" element={<LoginPage />} />
-      </Routes> 
-    </>
-  );
+    return (
+        <>
+            <h1>{message}</h1>
+            <InputContext.Provider
+                value={{
+                    inputs,
+                    handleInputChange: (inputName, inputValue) =>
+                        setInputs({ ...inputs, [inputName]: inputValue }),
+                }}
+            >
+                {/* EXAMPLE: How to add TextInput and DropdownInput
+                
+                <TextInput
+                    label="Text Input"
+                    id="testInput"
+                    type="text"
+                    placeholder="Enter text here"
+                    textarea={false}
+                />
+                <TextInput
+                    label="Text Area"
+                    id="textArea"
+                    type="textarea"
+                    placeholder="Enter text here"
+                    textarea={true}
+                />
+                <DropdownInput
+                    label="Dropdown Menu"
+                    id="DropdownMenu"
+                    options={options}
+                /> */}
+
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={
+                            <ProtectedRoute>
+                                <HomePage />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route path="/login" element={<LoginPage />} />
+                </Routes>
+            </InputContext.Provider>
+        </>
+    )
 }
 
-export default App;
+export default App
--- src/components/inputs/DropdownInput.js
+++ src/components/inputs/DropdownInput.js
@@ -1,30 +1,32 @@
-import { useContext } from "react";
-import { inputContext } from "../../App";
-import "./inputStyles.css";
+import { useContext } from 'react'
+import { inputContext } from '../../App'
+import './inputStyles.css'
 
-export default function Dropdowninput({ label, id, options }) {
-  const { inputs, handleInputChange } = useContext(inputContext);
+export default function DropdownInput({ label, id, options }) {
+    const { inputs, handleInputChange } = useContext(inputContext)
 
-  return (
-    <label className="text-input-label" htmlFor={id}>
-      {label}
-      <select
-        className="dropdown-select"
-        id={id}
-        name={id}
-        value={inputs[id]}
-        defaultValue=""
-        onChange={(e) => handleInputChange(e.target.name, e.target.value)}
-      >
-        <option value="" disabled>
-          Select your option
-        </option>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-    </label>
-  );
+    return (
+        <label className="text-input-label" htmlFor={id}>
+            {label}
+            <select
+                className="dropdown-select"
+                id={id}
+                name={id}
+                value={inputs[id]}
+                defaultValue=""
+                onChange={(e) =>
+                    handleInputChange(e.target.name, e.target.value)
+                }
+            >
+                <option value="" disabled>
+                    Select your option
+                </option>
+                {options.map((option) => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+        </label>
+    )
 }
