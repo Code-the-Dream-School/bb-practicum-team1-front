ec71c61fa1871a137dfee2325b7cadf75806c172
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-04-04 21:47:54 -0700

    edited fetch wrapper

--- src/util/fetch.ts
+++ src/util/fetch.ts
@@ -1,7 +1,7 @@
 import { error } from 'console'
 import {getCookie , setCookie, deleteCookie} from '../util/Authentication'
 
-export const fetchAPIData = async (url: string, method: string, body:object | undefined , headers?:object) => {
+export const fetchAPIData = async (url: string, method: string, body:object , headers?:object) => {
     const cookie = getCookie('shelf-share-session')as { token: string }
     const token = cookie?.token
     const response = await fetch(url , {
