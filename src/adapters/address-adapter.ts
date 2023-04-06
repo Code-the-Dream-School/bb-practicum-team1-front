4865d82c7619b00874899c53761b6db7fb53048f
bonivan <ibondarev@luxoft.com>
2023-04-05 22:28:01 -0600

    Adding exception handling

--- src/adapters/address-adapter.ts
+++ src/adapters/address-adapter.ts
@@ -4,11 +4,28 @@ type Address = {
     longitude: number,
 }
 
-const getAddressCoordinate = (address: string): Promise<Address> =>
+// Typescript Adapter for getAddressCoordinate route
+const getAddressCoordinate = (address: string): Promise<void | Address> => 
     fetch(`https://{process.env.HOSTNAME}/api/v1/address/search?address=${address}`, {
         method: 'get',
     })
     .then((resp) => {
         let result: Address = <Address><unknown>resp.json();
         return result;
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+    // Typescript Adapter for getAddressAutocomplete route
+const getAddressAutocomplete = (address: string): Promise<void | Array<Address>> => 
+    fetch(`https://{process.env.HOSTNAME}/api/v1/autocomplete/search?address=${address}`, {
+        method: 'get',
+    })
+    .then((resp) => {
+        let result: Array<Address> = <Array<Address>><unknown>resp.json();
+        return result;
+    })
+    .catch((e) => {
+        console.log(e);
     });
