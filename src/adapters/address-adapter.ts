34844305b5905c7cb2d2ac35cadb1a6b0dfe58fe
bonivan <ibondarev@luxoft.com>
2023-04-07 23:01:53 -0500

    Adding comments

--- src/adapters/address-adapter.ts
+++ src/adapters/address-adapter.ts
@@ -1,3 +1,4 @@
+// Declare Address type for object returned from backend API
 type Address = {
     address: string,
     latitude: number,
@@ -13,13 +14,21 @@ type Address = {
  * @returns {Promise<void | Address>} A promise that is top matching result for given address with coordinates..
  */
 export const getAddressCoordinate = (address: string): Promise<void | Address> => 
+    // Call backend API to search for given address. ${process.env.HOSTNAME} variable is used to avoid hardcoding hostname
+    // to allow running it locally or remotly without code changes.
+    // Value for HOSTNAME is provided in .env file
     fetch(`https://{process.env.HOSTNAME}/api/v1/address/search?address=${address}`, {
         method: 'get',
     })
+    // Parse response from server from json to object and cast it to object of our Address type.
+    // Cast to unknown is needed to workaround Typescript limitations of type conversions.
+    // Since we get object of unknown type Typescript doesn't allow to cast directly to Address type 
+    // and we have to cast to unknown type first
     .then((resp) => {
         let result: Address = <Address><unknown>resp.json();
         return result;
     })
+    // If there was an exception then we log it and return Void object
     .catch((e) => {
         console.log(e);
     });
@@ -33,13 +42,21 @@ export const getAddressCoordinate = (address: string): Promise<void | Address> =
  * @returns {Promise<void | Array<Address>>} A promise that is all adresses matches the given argument.
  */
 export const getAddressAutocomplete = (address: string): Promise<void | Array<Address>> => 
+    // Call backend API to get autocomplete suggestions for given address. ${process.env.HOSTNAME} variable is used to avoid hardcoding hostname
+    // to allow running it locally or remotly without code changes.
+    // Value for HOSTNAME is provided in .env file
     fetch(`https://{process.env.HOSTNAME}/api/v1/autocomplete/search?address=${address}`, {
         method: 'get',
     })
+    // Parse response from server from json to object and cast it to list of objects of our Address type.
+    // Cast to unknown is needed to workaround Typescript limitations of type conversions.
+    // Since we get object of unknown type Typescript doesn't allow to cast directly to Address type 
+    // and we have to cast to unknown type first
     .then((resp) => {
         let result: Array<Address> = <Array<Address>><unknown>resp.json();
         return result;
     })
+    // If there was an exception then we log it and return Void object
     .catch((e) => {
         console.log(e);
     });
