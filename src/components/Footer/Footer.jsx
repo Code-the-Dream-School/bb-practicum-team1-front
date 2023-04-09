00593bb5e32a4f2b5df1ee3c9b7ed086d53035e3
bonivan <ibondarev@luxoft.com>
2023-04-08 22:59:04 -0400

    FRONT-43 Footer - change github link to open in a new tab

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,12 +12,12 @@ const Footer = () => {
             <p className='copyright'>© 2023 BookSwap</p>
             <div className='git-links'>
                 <p>Follow Us on GitHub</p>
-                <a href="https://github.com/Manizha-khorram"><img src="https://ca.slack-edge.com/T07EHJ738-U03G77AKPFY-1878db5f4c82-512" alt="Manizha"/></a>
-                <a href="https://github.com/larasodjati"><img src="https://avatars.githubusercontent.com/u/108501073?v=4" alt="Lara"/></a>
-                <a href="https://github.com/Ekaterina-Bondareva"><img src="https://avatars.githubusercontent.com/u/107886514?v=4" alt="Kate"/></a>
-                <a href="https://github.com/cecerodrgz"><img src="https://ca.slack-edge.com/T07EHJ738-U02V69WEB98-81e1d9faa462-512" alt="Cecilia"/></a>
-                <a href="https://github.com/SimonjShurety"><img src="https://avatars.githubusercontent.com/u/102368650?v=4" alt="Simon"/></a>
-                <a href="https://github.com/koral14"><img src="https://avatars.githubusercontent.com/u/105457134?v=4" alt="Olga"/></a>
+                <a href="https://github.com/Manizha-khorram" target="_blank" rel="noreferrer"><img src="https://ca.slack-edge.com/T07EHJ738-U03G77AKPFY-1878db5f4c82-512" alt="Manizha"/></a>
+                <a href="https://github.com/larasodjati" target="_blank" rel="noreferrer"><img src="https://avatars.githubusercontent.com/u/108501073?v=4" alt="Lara"/></a>
+                <a href="https://github.com/Ekaterina-Bondareva" target="_blank" rel="noreferrer"><img src="https://avatars.githubusercontent.com/u/107886514?v=4" alt="Kate"/></a>
+                <a href="https://github.com/cecerodrgz" target="_blank" rel="noreferrer"><img src="https://ca.slack-edge.com/T07EHJ738-U02V69WEB98-81e1d9faa462-512" alt="Cecilia"/></a>
+                <a href="https://github.com/SimonjShurety" target="_blank" rel="noreferrer"><img src="https://avatars.githubusercontent.com/u/102368650?v=4" alt="Simon"/></a>
+                <a href="https://github.com/koral14" target="_blank" rel="noreferrer"><img src="https://avatars.githubusercontent.com/u/105457134?v=4" alt="Olga"/></a>
             </div>
         </div>
     )
