51b4533a410f91b6900a65a121d600e139dcbad1
SimonjShurety <simonjshurety@gmail.com>
2023-04-12 10:43:22 -0300

    loading spinner

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React, { useState, useEffect, createContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { getAllData } from './util/index';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import HomePage from './components/HomePage/HomePage';
-import LoginPage from './components/LoginPage/LoginPage';
-import CreateBook from './components/CreateBook/CreateBook';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
-import {setCookie, getCookie, deleteCookie} from './util/Authentication';
+import React, { useState, useEffect, createContext } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { getAllData } from './util/index'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import HomePage from './components/HomePage/HomePage'
+import LoginPage from './components/LoginPage/LoginPage'
+import CreateBook from './components/CreateBook/CreateBook'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
+import { setCookie, getCookie, deleteCookie } from './util/Authentication'
 import './sass/app.scss'
+import Flipbook from './components/LoadingSpinner/Flipbook'
 
-export const InputContext = createContext({});
+export const InputContext = createContext({})
 
 const URL = 'http://localhost:8000/api/v1/'
 
 const App = () => {
-  const [message, setMessage] = useState('')
-  const [inputs, setInputs] = useState({})
+    const [inputs, setInputs] = useState({})
+    const [loading, setLoading] = useState(false)
+    const [quote, setQuote] = useState({})
 
-  /* EXAMPLE: DropdownInput selection options
+    const getRandomQuote = () => {
+        setLoading(true)
+        setTimeout(() => {
+            fetch('https://api.quotable.io/random')
+                .then((res) => res.json())
+                .then((data) => {
+                    setLoading(false)
+                    setQuote(data)
+                })
+        }, 5000)
+    }
+    /* EXAMPLE: DropdownInput selection options
   
   const options = [
       { value: 'chocolate', label: 'Chocolate' },
@@ -27,32 +40,20 @@ const App = () => {
   ]
   */
 
-  useEffect(() => {
-      ;(async () => {
-          const myData = await getAllData(URL)
-          setMessage(myData.data)
-      })()
+    return (
+        <>
+            <Header />
+            <InputContext.Provider
+                value={{
+                    inputs,
+                    handleInputChange: (inputName, inputValue) =>
+                        setInputs({ ...inputs, [inputName]: inputValue }),
 
-      return () => {
-          console.log('unmounting')
-      }
-  }, [])
-
-
-  return (
-    <>
-      <Header />
-      <InputContext.Provider
-        value={{
-            inputs,
-            handleInputChange: (inputName, inputValue) =>
-                setInputs({ ...inputs, [inputName]: inputValue }),
-
-            handleBulkInput: (inputObj) => 
-                setInputs({ ...inputs, ...inputObj }),
-        }}
-      >
-        {/* EXAMPLE: How to add TextInput and DropdownInput
+                    handleBulkInput: (inputObj) =>
+                        setInputs({ ...inputs, ...inputObj }),
+                }}
+            >
+                {/* EXAMPLE: How to add TextInput and DropdownInput
         
         <TextInput
             label="Text Input"
@@ -82,30 +83,40 @@ const App = () => {
             options={options}
         /> */}
 
-        <Routes>
-          <Route 
-            exact 
-            path="/" 
-            element={
-              // <ProtectedRoute>
-                <HomePage />
-              // </ProtectedRoute>
-            } 
-          />
-          <Route  
-            path="/login" 
-            element={<LoginPage />} 
-          />
-          <Route  
-          path="/createBook" 
-          element={<CreateBook />} 
-        />
-      </Routes> 
-      </InputContext.Provider>  
-      <Footer />
-    </>
-
-  );
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={
+                            // <ProtectedRoute>
+                            <HomePage />
+                            // </ProtectedRoute>
+                        }
+                    />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/createBook" element={<CreateBook />} />
+                </Routes>
+            </InputContext.Provider>
+            <div>
+                <div className="buttons">
+                    <button className="btn get-quote" onClick={getRandomQuote}>
+                        Generate Quote
+                    </button>
+                </div>
+                {loading ? (
+                    <Flipbook />
+                ) : (
+                    <div className="quote-section">
+                        <blockquote className="quote">
+                            {quote.content}
+                        </blockquote>{' '}
+                        <span className="author">{quote.author}</span>
+                    </div>
+                )}
+            </div>
+            <Footer />
+        </>
+    )
 }
 
-export default App;
+export default App
