cb689537dbe28dd5aee2e2d8f0f231888965fe3b
bonivan <ibondarev@luxoft.com>
2023-04-13 19:35:25 -0400

    FRONT-35 Create Address Search Component

---
+++ src/components/AddressSearch/AddressSearch.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import DebouncedSearch from '../DebouncedSearch/DebouncedSearch';
+import { getAddressCoordinate, getAddressAutocomplete } from "../../adapters/address-adapter"; 
+
+const CreateAddress = ({ id }) => {
+    const [addressSuggestions, setAddressSuggestions] = useState([]);
+
+    const processSearch = (value) => {
+        //Unwrapping promise
+        getAddressAutocomplete(value).then(data => {
+            setAddressSuggestions(data)
+        })       
+    }
+
+    return (
+        <>
+            <DebouncedSearch id={id} handleDebounce={processSearch} />
+            <ul>
+                {addressSuggestions.map((item) => 
+                    <li id={item} value={item}>{item}</li>
+                )
+                }
+            </ul>
+        </>
+    );
+
+
+};
+
+export default CreateAddress;
