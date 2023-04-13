97a1fa9e37cdb5b520739bc7e16b68d6a3d243bb
bonivan <ibondarev@luxoft.com>
2023-04-12 21:57:59 -0400

    Fixing code review

--- src/adapters/address-adapter.ts
+++ src/adapters/address-adapter.ts
@@ -1,3 +1,5 @@
+import { fetchAPIData } from "../util/fetch";
+
 // Declare Address type for object returned from backend API
 type Address = {
     address: string,
@@ -13,25 +15,15 @@ type Address = {
  * getAddressCoordinate(address);
  * @returns {Promise<void | Address>} A promise that is top matching result for given address with coordinates..
  */
-export const getAddressCoordinate = (address: string): Promise<void | Address> => 
+
+export const getAddressCoordinate = async(address: string): Promise<void | Address> => {
     // Call backend API to search for given address. ${process.env.HOSTNAME} variable is used to avoid hardcoding hostname
     // to allow running it locally or remotly without code changes.
     // Value for HOSTNAME is provided in .env file
-    fetch(`https://{process.env.HOSTNAME}/api/v1/address/search?address=${address}`, {
-        method: 'get',
-    })
-    // Parse response from server from json to object and cast it to object of our Address type.
-    // Cast to unknown is needed to workaround Typescript limitations of type conversions.
-    // Since we get object of unknown type Typescript doesn't allow to cast directly to Address type 
-    // and we have to cast to unknown type first
-    .then((resp) => {
-        let result: Address = <Address><unknown>resp.json();
-        return result;
-    })
-    // If there was an exception then we log it and return Void object
-    .catch((e) => {
-        console.log(e);
-    });
+    const url = `http://localhost:8000/api/v1/address/search?address=${address}`
+    const data = await fetchAPIData(url, 'GET', undefined)
+    return data
+}
 
 /**
  * This function will return all adresses that match given argument.
@@ -41,22 +33,11 @@ export const getAddressCoordinate = (address: string): Promise<void | Address> =
  * getAddressAutocomplete(address);
  * @returns {Promise<void | Array<Address>>} A promise that is all adresses matches the given argument.
  */
-export const getAddressAutocomplete = (address: string): Promise<void | Array<Address>> => 
+export const getAddressAutocomplete = async(address: string): Promise<void | Array<Address>> => {
     // Call backend API to get autocomplete suggestions for given address. ${process.env.HOSTNAME} variable is used to avoid hardcoding hostname
     // to allow running it locally or remotly without code changes.
     // Value for HOSTNAME is provided in .env file
-    fetch(`https://{process.env.HOSTNAME}/api/v1/autocomplete/search?address=${address}`, {
-        method: 'get',
-    })
-    // Parse response from server from json to object and cast it to list of objects of our Address type.
-    // Cast to unknown is needed to workaround Typescript limitations of type conversions.
-    // Since we get object of unknown type Typescript doesn't allow to cast directly to Address type 
-    // and we have to cast to unknown type first
-    .then((resp) => {
-        let result: Array<Address> = <Array<Address>><unknown>resp.json();
-        return result;
-    })
-    // If there was an exception then we log it and return Void object
-    .catch((e) => {
-        console.log(e);
-    });
+    const url = `http://localhost:8000/api/v1/address/search?address=${address}`
+    const data = await fetchAPIData(url, 'GET', undefined)
+    return data
+}
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const LoginPage = () => {
-
     return (
         <div>
             <h1>This is the Login Page</h1>
