37d27291e4077b3fe4f0f69389577745ef04cfa0
bonivan <ibondarev@luxoft.com>
2023-04-20 18:43:32 -0400

    Adding inputs for book search

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import DebouncedSearch from '';
-import AddressSearch from '';
-import PaginatedResults from '';
+import React, { useState, useContext } from 'react';
+import DebouncedSearch from '../../util/DebouncedSearch/DebouncedSearch';
+import DropdownInput from '../../components/inputs/DropdownInput';
+// import PaginatedResults from '../PagePagination/PagePagination';
+import { getAllBooksAdapter } from '../../adapters/book-adapters';
+import { InputContext } from "../../App";
 
 const SearchPage = () => {
+    const [searchType, setSearchType] = useState('title');
+    const { inputs } = useContext(InputContext); 
+
+    const processSearch = (e) => {
+        let type = searchType;
+        let bookInput = {};
+        if (type === 'title') {
+            bookInput.title = inputs['searchType'];
+        } 
+        if (type === 'author') {
+            bookInput.author = inputs['searchType'];
+        }
+        getAllBooksAdapter(bookInput)
+        .then(data => console.log(data));
+    };
+
     return (
         <>
-            <DebouncedSearch />
-            <AddressSearch />
-            <PaginatedResults />
+            <DebouncedSearch 
+                id={'searchPageDebouncedSearch'}
+                handleDebounce={processSearch}
+            />
+            <DropdownInput 
+                label={'Choose:'}
+                id={'searchType'}
+                options={[{value: 'title', label: 'Search by title'}, {value: 'author', label: 'Search by author'}]}
+                defaultValue={'title'} showPlaceholder={false}
+            />
+            {/* <PaginatedResults /> */}
         </>
     );
 };
--- src/components/inputs/DropdownInput.js
+++ src/components/inputs/DropdownInput.js
@@ -2,9 +2,9 @@ import { useContext } from 'react'
 import { InputContext } from '../../App'
 import './inputStyles.css'
 
-export default function DropdownInput({ label, id, options }) {
+export default function DropdownInput({ label, id, options, defaultValue, showPlaceholder }) {
     const { inputs, handleInputChange } = useContext(InputContext)
-
+    console.log(options)
     return (
         <label className="text-input-label" htmlFor={id}>
             {label}
@@ -13,14 +13,17 @@ export default function DropdownInput({ label, id, options }) {
                 id={id}
                 name={id}
                 value={inputs[id]}
-                defaultValue=""
+                defaultValue={defaultValue}
                 onChange={(e) =>
                     handleInputChange(e.target.name, e.target.value)
                 }
             >
-                <option value="" disabled>
-                    Select your option
-                </option>
+                {showPlaceholder ? 
+                    <option value="" disabled>
+                        Select your option
+                    </option>
+                    : null
+                }
                 {options.map((option) => (
                     <option key={option.value} value={option.value}>
                         {option.label}
