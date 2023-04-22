6e5b114574bc69de28fe6e653ce274a42d1862dd
koral14 <105457134+koral14@users.noreply.github.com>
2023-04-21 23:43:49 -0700

    FRONT-63 added randomBooks function to the Home Page component

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,170 +1,174 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import BookList from '../BookList/BookList'
 import { Link } from 'react-router-dom'
+import { getAllBooksAdapter } from '../../adapters/book-adapters'
+import { InputContext } from '../../App';
+
+const bookList = [
+    {
+        title: 'Test',
+        language: 'English',
+        ageRange: 'kids',
+        publishingYear: 2022,
+        status: 'open',
+        image: true,
+        description: 'Colorful book with a lot of beautiful pictures',
+        genre: 'Literary Fiction',
+        author: 'Charles Perrault',
+    },
+    {
+        title: 'Cinderella',
+        language: 'English',
+        ageRange: 'kids',
+        publishingYear: 2022,
+        status: 'open',
+        image: false,
+        description: 'Colorful book with a lot of beautiful pictures',
+        genre: 'Literary Fiction',
+        author: 'Charles Perrault',
+    },
+    {
+        title: 'War and Peace',
+        language: 'Russian',
+        ageRange: 'adults',
+        publishingYear: 1988,
+        status: 'borrowed',
+        image: true,
+        description: 'The classic of world literature',
+        genre: 'Graphic Novel',
+        author: 'Leo Tolstoy',
+    },
+    {
+        title: 'War and Peace',
+        language: 'Russian',
+        ageRange: 'adults',
+        publishingYear: 1988,
+        status: 'open',
+        image: true,
+        description: 'The classic of world literature',
+        genre: 'Graphic Novel',
+        author: 'Leo Tolstoy',
+    },
+    {
+        title: 'War and Peace',
+        language: 'Russian',
+        ageRange: 'adults',
+        publishingYear: 1988,
+        status: 'open',
+        image: true,
+        description: 'The classic of world literature',
+        genre: 'Graphic Novel',
+        author: 'Leo Tolstoy',
+    },
+    {
+        title: 'War and Peace',
+        language: 'Russian',
+        ageRange: 'adults',
+        publishingYear: 1988,
+        status: 'borrowed',
+        image: true,
+        description: 'The classic of world literature',
+        genre: 'Graphic Novel',
+        author: 'Leo Tolstoy',
+    },
+    {
+        title: 'War and Peace',
+        language: 'Russian',
+        ageRange: 'adults',
+        publishingYear: 1988,
+        status: 'borrowed',
+        image: true,
+        description: 'The classic of world literature',
+        genre: 'Graphic Novel',
+        author: 'Leo Tolstoy',
+    },
+    {
+        title: 'War and Peace',
+        language: 'Russian',
+        ageRange: 'adults',
+        publishingYear: 1988,
+        status: 'open',
+        image: true,
+        description: 'The classic of world literature',
+        genre: 'Graphic Novel',
+        author: 'Leo Tolstoy',
+    },
+    {
+        title: 'War and Peace',
+        language: 'Russian',
+        ageRange: 'adults',
+        publishingYear: 1988,
+        status: 'borrowed',
+        image: true,
+        description: 'The classic of world literature',
+        genre: 'Graphic Novel',
+        author: 'Leo Tolstoy',
+    },
+    {
+        title: 'War and Peace',
+        language: 'Russian',
+        ageRange: 'adults',
+        publishingYear: 1988,
+        status: 'borrowed',
+        image: true,
+        description: 'The classic of world literature',
+        genre: 'Graphic Novel',
+        author: 'Leo Tolstoy',
+    },
+]
+
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomArr = (arr) => {
+    return arr[getRandom(0, arr.length - 1)];
+};
+
+// const get5RandomBooks = () => {
+//     return randomArr(bookList);
+// };
+
+// const get5RecentBooks = () => {
+//     const breed = randomArr(bookList);
+//     return breed.id;
+// };
+
+// const getRandomCatImage = async () => {
+//     return await getCatImage(getCatBreed());
+// }
 
 const HomePage = () => {
     //Sample array of book data (test)
-    const bookList = [
-        {
-            title: 'Cinderella',
-            language: 'English',
-            ageRange: 'kids',
-            publishingYear: 2022,
-            status: 'open',
-            image: true,
-            description: 'Colorful book with a lot of beautiful pictures',
-            genre: 'Literary Fiction',
-            author: 'Charles Perrault',
-        },
-        {
-            title: 'Cinderella',
-            language: 'English',
-            ageRange: 'kids',
-            publishingYear: 2022,
-            status: 'open',
-            image: false,
-            description: 'Colorful book with a lot of beautiful pictures',
-            genre: 'Literary Fiction',
-            author: 'Charles Perrault',
-        },
-        {
-            title: 'War and Peace',
-            language: 'Russian',
-            ageRange: 'adults',
-            publishingYear: 1988,
-            status: 'borrowed',
-            image: true,
-            description: 'The classic of world literature',
-            genre: 'Graphic Novel',
-            author: 'Leo Tolstoy',
-        },
-        {
-            title: 'War and Peace',
-            language: 'Russian',
-            ageRange: 'adults',
-            publishingYear: 1988,
-            status: 'open',
-            image: true,
-            description: 'The classic of world literature',
-            genre: 'Graphic Novel',
-            author: 'Leo Tolstoy',
-        },
-        {
-            title: 'War and Peace',
-            language: 'Russian',
-            ageRange: 'adults',
-            publishingYear: 1988,
-            status: 'open',
-            image: true,
-            description: 'The classic of world literature',
-            genre: 'Graphic Novel',
-            author: 'Leo Tolstoy',
-        },
-        {
-            title: 'War and Peace',
-            language: 'Russian',
-            ageRange: 'adults',
-            publishingYear: 1988,
-            status: 'borrowed',
-            image: true,
-            description: 'The classic of world literature',
-            genre: 'Graphic Novel',
-            author: 'Leo Tolstoy',
-        },
-        {
-            title: 'War and Peace',
-            language: 'Russian',
-            ageRange: 'adults',
-            publishingYear: 1988,
-            status: 'borrowed',
-            image: true,
-            description: 'The classic of world literature',
-            genre: 'Graphic Novel',
-            author: 'Leo Tolstoy',
-        },
-        {
-            title: 'War and Peace',
-            language: 'Russian',
-            ageRange: 'adults',
-            publishingYear: 1988,
-            status: 'open',
-            image: true,
-            description: 'The classic of world literature',
-            genre: 'Graphic Novel',
-            author: 'Leo Tolstoy',
-        },
-        {
-            title: 'War and Peace',
-            language: 'Russian',
-            ageRange: 'adults',
-            publishingYear: 1988,
-            status: 'borrowed',
-            image: true,
-            description: 'The classic of world literature',
-            genre: 'Graphic Novel',
-            author: 'Leo Tolstoy',
-        },
-        {
-            title: 'War and Peace',
-            language: 'Russian',
-            ageRange: 'adults',
-            publishingYear: 1988,
-            status: 'borrowed',
-            image: true,
-            description: 'The classic of world literature',
-            genre: 'Graphic Novel',
-            author: 'Leo Tolstoy',
-        },
-        {
-            title: 'Bible',
-            language: 'Chinese',
-            ageRange: 'adults',
-            publishingYear: 1901,
-            status: 'open',
-            image: false,
-            description:
-                'The Christian scriptures, consisting of the Old and New Testaments',
-            genre: 'Religion & Spirituality',
-            author: 'unknown',
-        },
-        {
-            title: 'Bible',
-            language: 'Chinese',
-            ageRange: 'adults',
-            publishingYear: 1901,
-            status: 'open',
-            image: false,
-            description:
-                'The Christian scriptures, consisting of the Old and New Testaments',
-            genre: 'Religion & Spirituality',
-            author: 'unknown',
-        },
-        {
-            title: 'Bible',
-            language: 'Chinese',
-            ageRange: 'adults',
-            publishingYear: 1901,
-            status: 'open',
-            image: false,
-            description:
-                'The Christian scriptures, consisting of the Old and New Testaments',
-            genre: 'Religion & Spirituality',
-            author: 'unknown',
-        },
-        {
-            title: 'Bible',
-            language: 'Chinese',
-            ageRange: 'adults',
-            publishingYear: 1901,
-            status: 'open',
-            image: false,
-            description:
-                'The Christian scriptures, consisting of the Old and New Testaments',
-            genre: 'Religion & Spirituality',
-            author: 'unknown',
-        },
-    ]
+    const { inputs, handleBulkInput } = useContext(InputContext);
 
+    const randomBooks = (max) => {
+        let random5Books = [];
+        for (let i = 0; i < max; i++) (
+            random5Books = randomArr (
+                getAllBooksAdapter(
+                    { 
+                        title: inputs.title, 
+                        author: inputs.author, 
+                        sort: inputs.sort,
+                        fields: inputs.fields,
+                        searchRadius: inputs.searchRadius,
+                        latitude: inputs.latitude,
+                        longitude: inputs.longitude,
+                        page: inputs.page,
+                        limit: inputs.limit,
+                        skip: inputs.skip
+                    }     
+                )
+            )  
+        )
+        return random5Books;
+    }
+
+    console.log('this is inputs', inputs)
+    console.log(randomBooks(5))
     return (
         <div className="homePage">
             {/* <h1 className='homePageCards'>Welcome to ShelfShare</h1> */}
