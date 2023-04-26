3a518c14fce1c4027ce399b16cf3cd765e983f54
Manizha-khorram <108601629+Manizha-khorram@users.noreply.github.com>
2023-04-26 02:30:06 -0400

    FRONT-88 adding baseURL in app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, { useState, useEffect, createContext } from 'react'
+
+// 3rd-party dependencies
 import { Routes, Route } from 'react-router-dom'
+
+// utility functions
 import { getAllData } from './util/index'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import DebouncedSearch from './util/DebouncedSearch/DebouncedSearch'
+import { setCookie, getCookie, deleteCookie } from './util/Authentication'
+
+// UI Components
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner'
+
+// Page components
 import HomePage from './components/HomePage/HomePage'
 import { Login } from './components/LoginPage/LoginPage'
 import { SignUp } from './components/SignupPage/SignUp'
+import LoginPage from './components/LoginPage/LoginPage'
 import CreateBook from './components/CreateBook/CreateBook'
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
-import { setCookie, getCookie, deleteCookie } from './util/Authentication'
+
 import './sass/app.scss'
-import { PagePagination } from './components/PagePagination/Pagination'
-
-export const InputContext = createContext({})
-
-export const SessionContext = createContext({});
+import About from './components/About/About'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import BookItem from './components/BookItem/BookItem'
+import SingleBook from './components/SingleBook/SingleBook'
 
 const testBook = {
     title: 'Cinderella',
@@ -29,49 +39,10 @@ const testBook = {
     author: 'Charles Perrault',
 }
 
-//This is for testing//
-const bookArr = [
-    {
-        title: 'Pride and Prejudice',
-        description:
-            'It is a truth universally acknowledged that when most people think of Jane Austen they think of this charming and humorous story of love, difficult families and the tricky task of finding a handsome husband with a good fortune.',
-        genre: 'Romance',
-        author: 'Jane Austen',
-    },
-    {
-        title: 'The Lion, the Witch and the Wardrobe',
-        description:
-            "C.S. Lewis's timeless tale captured the hearts of children everywhere with its fantastical world through the wardrobe, full of fauns, dwarves and anthropomorphised animals. Whether you were Peter, Edmund, Susan or Lucy, we all wanted to put on a fur coat and go on a snow-laden adventure with Mr Tumnus.",
-        genre: 'Novel',
-        author: 'C.S. Lewis',
-    },
-    {
-        title: 'Frankenstein',
-        description:
-            'The book tells the story of Victor Frankenstein, a Swiss student of natural science who creates an artificial man from pieces of corpses and brings his creature to life.',
-        genre: 'Gothic Fiction',
-        author: 'Mary Shelley',
-    },
-]
+export const InputContext = createContext({})
+export const SessionContext = createContext({});
 
-const bigBookArray = [
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-    ...bookArr,
-]
-
-const URL = 'http://localhost:8000/api/v1/'
+const baseURL = window.location.hostname === 'localhost'? 'http://localhost:8000/api/v1/': 'https://shelf-share.onrender.com';
 
 const App = () => {
     const [message, setMessage] = useState('')
@@ -79,7 +50,6 @@ const App = () => {
     const [sessionObject, setSessionObject] = useState(getCookie());
     const [loading, setLoading] = useState(false)
     const [quote, setQuote] = useState({})
-    const [night, setNight] = useState(false);
 
     const getRandomQuote = () => {
         setLoading(true)
@@ -102,16 +72,16 @@ const App = () => {
   ]
   */
 
-    useEffect(() => {
-        ;(async () => {
-            const myData = await getAllData(URL)
-            setMessage(myData.data)
-        })()
+    // useEffect(() => {
+    //     ;(async () => {
+    //         const myData = await getAllData(URL)
+    //         setMessage(myData.data)
+    //     })()
 
-        return () => {
-            console.log('unmounting')
-        }
-    }, [])
+    //     return () => {
+    //         console.log('unmounting')
+    //     }
+    // }, [])
 
     return (
         <>
@@ -129,24 +99,22 @@ const App = () => {
                                 setInputs({ ...inputs, ...inputObj }),
                         }}
                     >
-                    <Header night={night} setNight={setNight} />
-                    <div className={!night ? "" : "night-mode-bg"}>
+                    <Header />
                     <Routes>
                         <Route path="" element={<HomePage />} />
-                        <Route path="/login" element={<Login setSessionObject={setSessionObject} />} />
+                        <Route path="/login" element={<Login />} />
                         <Route path="/sign-up" element={<SignUp setSessionObject={setSessionObject} />} />
-                        {/* <Route path="/about" element={<About />} /> */}
+                        <Route path="/about" element={<About />} />
                         <Route path="/books/create" element={<CreateBook />} />
                         <Route
                             path="/books/edit/:bookId"
                             element={<CreateBook />}
                         />
-                        {/* <Route
+                        <Route
                             path="/books/:bookId"
                             element={<SingleBook item={testBook} />}
-                        /> */}
+                        />
                         </Routes>
-                        </div>
                     </InputContext.Provider>
                 </SessionContext.Provider>
                 <Footer />
@@ -159,7 +127,7 @@ const App = () => {
                             Loading Spinner Quote Button (click here)
                         </button>
                     </div>
-                    {/* {loading ? (
+                    {loading ? (
                         <LoadingSpinner />
                     ) : (
                         <div className="quote-section">
@@ -168,7 +136,7 @@ const App = () => {
                             </blockquote>{' '}
                             <span className="author">{quote.author}</span>
                         </div>
-                    )} */}
+                    )}
                 </div>
             </div>
         </>
