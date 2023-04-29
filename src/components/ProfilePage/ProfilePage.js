7e21a454ba008da7ae1137d80cf51f720a73dc5f
Cecilia Rodriguez <cecerodrgz@Cecilias-Air.lan>
2023-04-28 20:27:47 -0400

    added profile information

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -4,37 +4,39 @@ import './Profile.scss'
 import { getAllBooksOwnerAdapter } from "../../adapters/book-adapters";
 import { SessionContext } from "../../App";
 import { PagePagination } from "../PagePagination/Pagination";
-import { loginAdapter } from "../../adapters/auth-adapters";
-import { Login } from "../LoginPage/LoginPage";
+
+
 
 const ProfilePage = ({myProfile = false}) => {
-    const [userData, setUserData] = useState({});
     const [usersBooks, setUsersBooks] = useState([]);
+
     const fetchUserBooks = async () => {
        const booksData = await getAllBooksOwnerAdapter()
        setUsersBooks(booksData.books)
     }
-    const fetchUser = async () => {
-      const userData = await loginAdapter()
-      setUserData(userData.user)
-    }
+
 
     useEffect( () => {
         fetchUserBooks()
-        fetchUser()
     }, [])
 
     const {sessionObject} = useContext(SessionContext)
     console.log(sessionObject)
 
-
     return (
         <div className="Profile">
           <h1>My Profile</h1>
+          <div className="user">
+            {sessionObject.user.givenName}<br/>
+            {sessionObject.user.familyName}<br/>
+            {sessionObject.user.username}<br/>
+            {sessionObject.user.address}<br/>
+            {sessionObject.user.dateOfBirth}<br/>
+          </div>
           <BookList bookList={usersBooks} />
-          <Login setSessionObject={userData} />
           <PagePagination bookList={usersBooks}/>
         </div>
+
       );
 }
 
