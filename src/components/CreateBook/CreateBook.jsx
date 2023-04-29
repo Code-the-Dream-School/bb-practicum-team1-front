da794b6a3f5208b21c6a1565fe0e32c3e4ed3bcd
koral14 <105457134+koral14@users.noreply.github.com>
2023-04-29 09:27:58 -0700

    adjusted the input boxes

--- src/components/CreateBook/CreateBook.jsx
+++ src/components/CreateBook/CreateBook.jsx
@@ -134,6 +134,7 @@ const CreateBook = ({ bookId }) => {
     return (
         <>
             <h1 className='h1-createBook'>Your {<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>} here</h1>
+            <div className='container-create-form'>
             <form className='createBookForm' onSubmit={handleFormSubmit}>
                 <div className='inputFields'>
                     <TextInput 
@@ -218,7 +219,9 @@ const CreateBook = ({ bookId }) => {
                                         }}
                                         />
                                     </label>
-                                    </div> : <div className='url-field'><TextInput type='text' placeholder='URL here...' id='urlField' /><button onClick={console.log('uploaded link')} className='plus-sign'>{plus}</button></div>
+                                    </div> : <div className='url-field'><TextInput type='text' placeholder='URL here...' id='urlField' />
+                                                <button onClick={console.log('uploaded link')} className='plus-sign'>{plus}</button>
+                                            </div>
                                 }
                             </div>
                         <button className='cover-toggle' onClick={handleURLToggle}>{urlButton ? 'Provide URL instead' : 'Upload Cover Image Instead'}</button>
@@ -228,6 +231,7 @@ const CreateBook = ({ bookId }) => {
                     <button className='addButton' title='Press to Add'>Add your {addButton}</button>
                 </div>
             </form>
+            </div>
         </>
     )
 }
