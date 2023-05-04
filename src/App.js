688a9eb442bb6f2c0df9b1ad7a149d0b1df1671d
Manizha-khorram <108601629+Manizha-khorram@users.noreply.github.com>
2023-05-04 18:06:50 -0400

    BACK-82 updated app.js

--- src/App.js
+++ src/App.js
@@ -1,146 +1,95 @@
-import React, { useState, useEffect, createContext } from 'react'
+import React, { useState, createContext } from 'react'
 
 // 3rd-party dependencies
 import { Routes, Route } from 'react-router-dom'
 
 // utility functions
-import { getAllData } from './util/index'
-import DebouncedSearch from './util/DebouncedSearch/DebouncedSearch'
-import { setCookie, getCookie, deleteCookie } from './util/Authentication'
-// UI Components
-import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner'
+import {
+    getCookie,
+    cookieName,
+} from './util/Authentication'
+
 
 // Page components
 import HomePage from './components/HomePage/HomePage'
 import { Login } from './components/LoginPage/LoginPage'
 import { SignUp } from './components/SignupPage/SignUp'
-import LoginPage from './components/LoginPage/LoginPage'
 import CreateBook from './components/CreateBook/CreateBook'
+import SingleBook from './components/SingleBook/SingleBook'
+import ProfilePage  from './components/ProfilePage/ProfilePage'
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
+import SearchPage from './components/SearchPage/SearchPage'
+
 import './sass/app.scss'
 import About from './components/About/About'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import BookItem from './components/BookItem/BookItem'
-import SingleBook from './components/SingleBook/SingleBook'
-const testBook = {
-    title: 'Cinderella',
-    language: 'English',
-    ageRange: 'kids',
-    publishingYear: 2022,
-    status: 'open',
-    image: true,
-    description:
-        "Catrina and her family are moving to the coast of Northern California because her little sister, Maya, is sick. Cat isn't happy about leaving her friends for Bahía de la Luna, but Maya has cystic fibrosis and will benefit from the cool, salty air that blows in from the sea. As the girls explore their new home, a neighbor lets them in on a secret: There are ghosts in Bahía de la Luna. Maya is determined to meet one, but Cat wants nothing to do with them. As the time of year when ghosts reunite with their loved ones approaches, Cat must figure out how to put aside her fears for her sisters sake -- and her own.Raina Telgemeier has masterfully created a moving and insightful story about the power of family and friendship, and how it gives us the courage to do what we never thought possible.",
-    genre: 'Literary Fiction',
-    author: 'Charles Perrault',
-}
+import Chat from './components/Chat/Chat'
+import AllConversations from './components/Chat/AllConversations'
+
 
 export const InputContext = createContext({})
-export const SessionContext = createContext({});
+export const SessionContext = createContext({
+    sessionObject: null,
+    setSessionObect: () => {},
+})
 
 const URL = 'http://localhost:8000/api/v1/'
+
+/**
+ *  level 1 - some kinda of state - dark/light mode   Provider (parent)
+ *  level 6 dark/light mode (useContext)
+ */
 
 const App = () => {
     const [message, setMessage] = useState('')
     const [inputs, setInputs] = useState({})
-    const [sessionObject, setSessionObject] = useState(getCookie());
-    const [loading, setLoading] = useState(false)
-    // const [quote, setQuote] = useState({})
     const [night, setNight] = useState(false);
-    // const getRandomQuote = () => {
-    //     setLoading(true)
-    //     setTimeout(() => {
-    //         fetch('https://api.quotable.io/random')
-    //             .then((res) => res.json())
-    //             .then((data) => {
-    //                 setLoading(false)
-    //                 setQuote(data)
-    //             })
-    //     }, 5000)
-    // }
-
-    /* EXAMPLE: DropdownInput selection options
-  
-  const options = [
-      { value: 'chocolate', label: 'Chocolate' },
-      { value: 'strawberry', label: 'Strawberry' },
-      { value: 'vanilla', label: 'Vanilla' },
-  ]
-  */
-
-    // useEffect(() => {
-    //     ;(async () => {
-    //         const myData = await getAllData(URL)
-    //         setMessage(myData.data)
-    //     })()
-
-    //     return () => {
-    //         console.log('unmounting')
-    //     }
-    // }, [])
+    const [sessionObject, setSessionObject] = useState(getCookie(cookieName))
+    const [urlButton, setUrlButton] = useState(false);
 
     return (
         <>
-            <div className="content">
-                <div className={!night ? "" : "night-mode-bg"}>
-                <SessionContext.Provider
-                    value={{sessionObject, setSessionObject}}
-                >
-                    <InputContext.Provider
-                        value={{
-                            inputs,
-                            handleInputChange: (inputName, inputValue) =>
-                                setInputs({ ...inputs, [inputName]: inputValue }),
+            <SessionContext.Provider value={{ sessionObject, setSessionObject }} >
+                <InputContext.Provider
+                    value={{
+                        inputs,
+                        handleInputChange: (inputName, inputValue) =>
+                            setInputs({ ...inputs, [inputName]: inputValue }),
 
-                            handleBulkInput: (inputObj) =>
-                                setInputs({ ...inputs, ...inputObj }),
-                        }}
-                    >
+                        handleBulkInput: (inputObj) =>
+                            setInputs({ ...inputs, ...inputObj }),
+                    }}
+                >
                     <Header night={night} setNight={setNight} />
-                        <Routes>
-                            <Route path="" element={<HomePage />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/sign-up" element={<SignUp setSessionObject={setSessionObject} />} />
-                            <Route path="/about" element={<About />} />
-                            <Route path="/books/create" element={<CreateBook />} />
-                            <Route
-                                path="/books/edit/:bookId"
-                                element={<CreateBook />}
-                            />
-                            <Route
-                                path="/books/:bookId"
-                                element={<SingleBook item={testBook} />}
-                            />
-                        </Routes>
-                    
-                    </InputContext.Provider>
-                </SessionContext.Provider>
-                </div>
-                <Footer />
-                {/* <div> */}
-                    {/* <div className="buttons">
-                        <button
-                            className="btn get-quote"
-                            onClick={getRandomQuote}
-                        >
-                            Loading Spinner Quote Button (click here)
-                        </button>
-                    </div> */}
-                    {/* {loading ? (
-                        <LoadingSpinner />
-                    ) : (
-                        <div className="quote-section">
-                            <blockquote className="quote">
-                                {quote.content}
-                            </blockquote>{' '}
-                            <span className="author">{quote.author}</span>
+                    <div className="content">
+                        <div className={!night ? "day-mode-bg" : "night-mode-bg"}>
+                            <Routes>
+                                <Route path="" element={<HomePage />} />
+                                <Route path="/login" element={<Login setSessionObject={setSessionObject} />} />
+                                <Route path="/sign-up" element={<SignUp setSessionObject={setSessionObject} />} />
+                                <Route path="/about" element={<About />} />
+                                <Route path="/search" element={<SearchPage/>} />
+                                <Route 
+                                        path="/books/create" 
+                                        element={<CreateBook 
+                                                    urlButton={urlButton}
+                                                    setUrlButton={setUrlButton}
+                                                />} />
+                                <Route path="/books/edit/:bookId" element={<CreateBook />} />
+                                <Route path="/books/:bookId" element={<SingleBook />} />
+                                //Using one component to show all user's conversations and another for a single conversation
+                                <Route path="/chat/:recipientId" element={<Chat/>} />
+                                <Route path="/chat/" element={<AllConversations/>} />
+                                <Route path="/my-profile" element={<ProfilePage />} />
+                            </Routes>
                         </div>
-                    )} */}
-                {/* </div> */}
-            </div>
+                    </div>
+                </InputContext.Provider>
+            </SessionContext.Provider>
+            <Footer />
         </>
     )
 }
 
-export default App
+export default App;
