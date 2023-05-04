596cd763caff3b5ac53ec21e385a8b176f535e33
bonivan <ibondarev@luxoft.com>
2023-05-03 23:29:23 -0400

    Added usernames instead of user ids

--- src/components/SingleBook/SingleBook.jsx
+++ src/components/SingleBook/SingleBook.jsx
@@ -5,6 +5,7 @@ import NoPic from '../images/Image-Not-Available.png';
 import ZeroPlus from '../images/zeroPlus.png';
 import { getSingleBookAdapter } from '../../adapters/book-adapters';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const SingleBook = () => {
   const routeParams = useParams();
