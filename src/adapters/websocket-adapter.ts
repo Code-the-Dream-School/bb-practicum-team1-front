a083be23b9830abf4502f9e9861679995d25f354
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-05-04 10:17:52 -0700

    Updated code on typing status websocket adapter

--- src/adapters/websocket-adapter.ts
+++ src/adapters/websocket-adapter.ts
@@ -1,5 +1,9 @@
 import {io, Socket} from 'socket.io-client'
-const socketURL = window.location.hostname === 'localhost'? 'ws://localhost:8000/api/v1/': 'ws://shelf-share.onrender.com/api/v1/'
+import { getCookie, cookieName } from '../util/Authentication';
+const socketURL = window.location.hostname === 'localhost'? 'ws://localhost:8000': 'wss://shelf-share.onrender.com'
+
+const jwtCookie = getCookie(cookieName)as { token: string }
+const jwtToken = jwtCookie ? jwtCookie.token: null;
 
 // Typing status adapter
 interface EventListeners {
@@ -8,17 +12,23 @@ interface EventListeners {
 /**
  * @param {EventListeners} eventListeners - An object with optional properties that will be called when certain event occurs
  * @example
+ * const url = `${socketURL}`
  * const eventListeners = {
  *   typingStatus: (typing) => {
- *     console.log(`The user is ${typing ? 'typing' : 'not typing'}`);
+ *     console.log(`The user is ${typing ? 'typing' : 'not typing'}`)
  *   }
- * };
+ * }
  * userTypingStatusAdapter(eventListeners)
  */
 export const userTypingStatusAdapter = async (eventListeners:EventListeners) => {
-    const socket : Socket = io(socketURL)
+    const socket: Socket = io(`${socketURL}/?token=${jwtToken}`)
+
+    socket.on('connect' , () =>{
+        console.log('Connected to the server!')
+      })
 
     socket.on('typingStatus', typing =>{
+
         if(eventListeners.typingStatus){
             eventListeners.typingStatus(typing)
         }
