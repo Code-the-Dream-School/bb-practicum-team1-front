b6edca4a8ad682e1572fba0c8278c97c8a153757
SimonjShurety <simonjshurety@gmail.com>
2023-05-05 09:44:49 -0300

    SingleBook suggestions

--- src/components/SingleBook/SingleBook.jsx
+++ src/components/SingleBook/SingleBook.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Message from '../images/message.png';
 import Adults from '../images/18plus1.png';
 import NoPic from '../images/Image-Not-Available.png';
@@ -6,87 +6,101 @@ import ZeroPlus from '../images/zeroPlus.png';
 import { getSingleBookAdapter } from '../../adapters/book-adapters';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { LoadingContext } from '../../App'
+import { LoadingContext } from '../../App';
 
 const SingleBook = () => {
-  const routeParams = useParams();
-  const [bookInformation, setBookInformation] = useState({});
-  const adult = bookInformation.ageRange === 'adults';
-  const description = bookInformation.description;
-  const noImg = bookInformation.imageURL === false;
-  const image = bookInformation.imageURL;
-  const language = bookInformation.language;
-  const author = bookInformation.author;
-  const title = bookInformation.title;
-  const genre = bookInformation.genre;
-  const publishingYear = bookInformation.publishingYear;
-  const { loading, setLoading } = useContext(LoadingContext)
+	const [errorMessage, setErrorMessage] = useState('');
+	const [bookInformation, setBookInformation] = useState({});
+	const [isError, setIsError] = useState(false);
 
+	const { id: bookId } = useParams();
+	const { loading, setLoading } = useContext(LoadingContext);
 
-  // getting the single book using useParams
-  const SingleFetchedBook = async () => {
-    if (routeParams.bookId) {
-      setLoading(true)
-      const newBook = await getSingleBookAdapter(routeParams.bookId);
-      setLoading(false)
-      setBookInformation(newBook);
-    }
+	const adult = bookInformation.ageRange === 'adults';
+	const description = bookInformation.description;
+	const noImg = bookInformation.imageURL === false;
+	const image = bookInformation.imageURL;
+	const language = bookInformation.language;
+	const author = bookInformation.author;
+	const title = bookInformation.title;
+	const genre = bookInformation.genre;
+	const publishingYear = bookInformation.publishingYear;
 
-    // displaying the book when the page refreshes
-    useEffect(() => {
-        SingleFetchedBook()
-    }, [routeParams.bookId])
+	// getting the single book using useParams
+	const fetchSingleBookById = async (id) => {
+		if (!id) {
+			setIsError(true);
+			setErrorMessage('There is no book ID input');
+		}
 
-    return (
-        <div className="container-single">
-            <div className="book-item-single">
-                <p className="kidsAdults-single">
-                    {adult ? (
-                        <img
-                            src={Adults}
-                            alt="Adults"
-                            title="Not appropriate for under 13 years old"
-                        />
-                    ) : (
-                        <img src={ZeroPlus} alt="Kids" title="Kid friendly" />
-                    )}
-                </p>
-                {noImg ? (
-                    <img
-                        src={NoPic}
-                        alt="No_Picture_available"
-                        className="coverImage-single"
-                    />
-                ) : (
-                    <img
-                        src={image}
-                        alt="Cover image"
-                        className="coverImage-single"
-                    />
-                )}
-                <div className="book-item-body-single">
-                    <div>
-                        <p className="titleAndYear-single">
-                            {title} ({publishingYear})
-                        </p>
-                        <Link
-                            to={`/chat/${bookInformation?.owner?._id}`}
-                            className="link-to-owner"
-                            data-id="Contact the owner"
-                        >
-                            <img src={Message} alt="message_me" />
-                        </Link>
-                    </div>
-                    <p className="description-single">{description}</p>
-                    <p className="author-p-single">Author: {author}</p>
-                    <p className="lang-p-single">Language: {language}</p>
-                    <p className="genre-p-single">Genre: {genre}</p>
-                </div>
-            </div>
-        </div>
-    )
-                }
-}
+		if (id) {
+			setLoading(true);
+			const newBook = await getSingleBookAdapter(id);
+			setBookInformation(newBook);
+			setLoading(false);
+		}
+	};
 
+	// displaying the book when the page refreshes
+	useEffect(() => {
+		fetchSingleBookById(bookId);
+	}, [bookId]);
 
-export default SingleBook
+	return (
+		<div className='container-single'>
+			<div className='book-item-single'>
+				{isError && <p style={{ color: 'red' }}>{errorMessage}</p>}
+				<p className='kidsAdults-single'>
+					{adult ? (
+						<img
+							src={Adults}
+							alt='Adults'
+							title='Not appropriate for under 13 years old'
+						/>
+					) : (
+						<img
+							src={ZeroPlus}
+							alt='Kids'
+							title='Kid friendly'
+						/>
+					)}
+				</p>
+				{noImg ? (
+					<img
+						src={NoPic}
+						alt='No_Picture_available'
+						className='coverImage-single'
+					/>
+				) : (
+					<img
+						src={image}
+						alt='Cover image'
+						className='coverImage-single'
+					/>
+				)}
+				<div className='book-item-body-single'>
+					<div>
+						<p className='titleAndYear-single'>
+							{title} ({publishingYear})
+						</p>
+						<Link
+							to={`/chat/${bookInformation?.owner?._id}`}
+							className='link-to-owner'
+							data-id='Contact the owner'>
+							<img
+								src={Message}
+								alt='message_me'
+							/>
+						</Link>
+					</div>
+					<p className='description-single'>{description}</p>
+					<p className='author-p-single'>Author: {author}</p>
+					<p className='lang-p-single'>Language: {language}</p>
+					<p className='genre-p-single'>Genre: {genre}</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SingleBook;
